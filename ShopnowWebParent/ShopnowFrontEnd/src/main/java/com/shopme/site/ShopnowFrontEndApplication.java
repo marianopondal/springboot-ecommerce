a48666c1376da25fcd7fe11128a12b64f4e08896
Mariano Pondal <marianopondal@gmail.com>
2022-06-20 22:22:28 -0300

    initial commit

---
+++ ShopnowWebParent/ShopnowFrontEnd/src/main/java/com/shopme/site/ShopnowFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnowFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopnowFrontEndApplication.class, args);
+	}
+
+}
